920390199147f065e1e57f9a4062850c73792716
Jactry <jactry92@gmail.com>
2012-09-28 00:14:31 +0800

    I Believe

---
+++ ibelieve/pitches.h
@@ -0,0 +1,100 @@
+
+#define DL1 147
+#define DL2 165
+#define DL3 175
+#define DL4 196
+#define DL5 221
+#define DL6 248
+#define DL7 278
+
+#define D0 -1
+#define D1 294
+#define D2 330
+#define D3 350
+#define D4 393
+#define D5 441
+#define D6 495
+#define D7 556
+
+#define DH1 589
+#define DH2 661
+#define DH3 700
+#define DH4 786
+#define DH5 882
+#define DH6 990
+#define DH7 1112
+
+#define EL1 165
+#define EL2 175
+#define EL3 196
+#define EL4 221
+#define EL5 248
+#define EL6 278
+#define EL7 312
+
+#define E0 -1
+#define E1 330 
+#define E2 350
+#define E3 393
+#define E4 441
+#define E5 495
+#define E6 556
+#define E7 624
+
+#define EH1 661
+#define EH2 700
+#define EH3 786
+#define EH4 882
+#define EH5 990
+#define EH6 1112
+#define EH7 1248
+
+#define FL1 175
+#define FL2 196 
+#define FL3 221
+#define FL4 234
+#define FL5 262
+#define FL6 294
+#define FL7 330
+
+#define F0 -1
+#define F1 350
+#define F2 393
+#define F3 441
+#define F4 495
+#define F5 556
+#define F6 624
+#define F7 661
+
+#define FH1 700
+#define FH2 882
+#define FH3 990
+#define FH4 1049
+#define FH5 1178
+#define FH6 1322
+#define FH7 1484
+
+#define GL1 196
+#define GL2 221
+#define GL3 234
+#define GL4 262
+#define GL5 294
+#define GL6 330
+#define GL7 371
+
+#define G0 -1
+#define G1 393
+#define G2 441
+#define G3 495
+#define G4 556
+#define G5 624
+#define G6 661
+#define G7 742
+
+#define GH1 786
+#define GH2 882
+#define GH3 990
+#define GH4 1049
+#define GH5 1178
+#define GH6 1322
+#define GH7 1484
